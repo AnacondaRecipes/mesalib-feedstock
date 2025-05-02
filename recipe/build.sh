10dd0d4cef4e995483c01a3b3b0d4f318fe65f9a
Serhii Kupriienko <skupriienko@anaconda.com>
2025-05-02 11:42:47 +0300

    25.0.5

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,39 +1,46 @@
 #!/bin/bash
 
-osname=`uname`
-if [ $osname == Linux ]; then
-    export CC="gcc"
-    export CXX="g++"
-elif [ $osname == Darwin ]; then
-    export CC="clang"
-    export CXX="clang++"
+set -ex
+
+export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$BUILD_PREFIX/lib/pkgconfig
+export PKG_CONFIG=$BUILD_PREFIX/bin/pkg-config
+
+if [[ $CONDA_BUILD_CROSS_COMPILATION == "1" ]]; then
+  if [[ "${CMAKE_CROSSCOMPILING_EMULATOR:-}" == "" ]]; then
+    # Mostly taken from https://github.com/conda-forge/pocl-feedstock/blob/b88046a851a95ab3c676c0b7815da8224bd66a09/recipe/build.sh#L52
+    rm $PREFIX/bin/llvm-config
+    cp $BUILD_PREFIX/bin/llvm-config $PREFIX/bin/llvm-config
+    export LLVM_CONFIG=${PREFIX}/bin/llvm-config
+  else
+    # https://github.com/mesonbuild/meson/issues/4254
+    export LLVM_CONFIG=${BUILD_PREFIX}/bin/llvm-config
+  fi
 fi
 
-export PKG_CONFIG_PATH="${PREFIX}/lib/pkgconfig/:"${PKG_CONFIG_PATH}
-export CFLAGS="-I${PREFIX}/include "${CFLAGS}
-export LDFLAGS="-L${PREFIX}/lib "${LDFLAGS}
+meson setup builddir/ \
+  ${MESON_ARGS} \
+  -Dplatforms=x11 \
+  -Dgles1=disabled \
+  -Dgles2=disabled \
+  -Dgallium-va=disabled \
+  -Dgbm=disabled \
+  -Dgallium-vdpau=disabled \
+  -Dshared-glapi=enabled \
+  -Dgallium-drivers=softpipe,llvmpipe \
+  -Degl=disabled \
+  -Dglx=disabled \
+  -Dllvm=enabled \
+  -Dshared-llvm=enabled \
+  -Dlibdir=lib \
+  -Dosmesa=true \
+  -Dvulkan-drivers=swrast \
+  -Dopengl=true \
+  -Dglx-direct=false \
+  || { cat builddir/meson-logs/meson-log.txt; exit 1; }
 
-./configure \
-    --prefix=$PREFIX \
-    --disable-gles1 \
-    --disable-gles2 \
-    --disable-va \
-    --disable-gbm \
-    --disable-xvmc \
-    --disable-vdpau \
-    --enable-shared-glapi \
-    --enable-texture-float \
-    --disable-dri \
-    --with-dri-drivers= \
-    --with-gallium-drivers=swrast \
-    --disable-egl \
-    --with-egl-platforms= \
-    --enable-gallium-osmesa \
-    --disable-glx \
-    --enable-llvm \
-    --disable-llvm-shared-libs \
-    --with-osmesa-bits=32
+ninja -C builddir/ -j ${CPU_COUNT}
 
-make -j${CPU_COUNT}
-make install
+ninja -C builddir/ install
 
+# meson test -C builddir/ \
+#   -t 4
