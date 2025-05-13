81654feb900c59007a8710e97b21f77b12045ad7
Serhii Kupriienko <skupriienko@anaconda.com>
2025-05-13 11:05:08 +0300

    Debug on linux

--- recipe/build.sh
+++ recipe/build.sh
@@ -22,14 +22,21 @@ if [[ "$target_platform" == linux* ]]; then
   GLVND_OPTION="-Dglvnd=enabled"
   GBM_OPTION="-Dgbm=enabled"
   VULKAN_DRIVERS="-Dvulkan-drivers=swrast,virtio"  # Linux compatible drivers only
+  # libclc is a required dependency for OpenCL support, which is used by some Gallium drivers like "rusticl" (the Rust OpenCL implementation).
+  # Mesa automatically tries to also enable OpenCL support, which needs the libclc library. 
+  # But libclc isn't available on the main channel:
+  # "Dependency 'libclc' not found, tried pkgconfig".
+  GALLIUM_DRIVERS="-Dgallium-drivers=swrast,virgl,llvmpipe,softpipe,zink"  # no rusticl
 elif [[ "$target_platform" == osx* ]]; then
   # macOS has limited Vulkan support
   VULKAN_DRIVERS="-Dvulkan-drivers=swrast"
   # On osx platfroms: meson.build:458:3: ERROR: Feature gbm cannot be enabled: GBM only supports DRM/KMS platforms
   GBM_OPTION="-Dgbm=disabled"
+  GALLIUM_DRIVERS="-Dgallium-drivers=swrast,llvmpipe,softpipe"
 else
   GLVND_OPTION="-Dglvnd=disabled"
   VULKAN_DRIVERS="-Dvulkan-drivers=all"  # Keep all for other platforms
+  GALLIUM_DRIVERS="-Dgallium-drivers=all"
 fi
 
 
@@ -51,7 +58,7 @@ meson setup builddir/ \
   -Dlibdir=lib \
   -Dplatforms=x11 \
   $VULKAN_DRIVERS \
-  -Dgallium-drivers=all \
+  $GALLIUM_DRIVERS \
   -Dgallium-va=disabled \
   -Dgallium-vdpau=disabled \
   -Dgles1=disabled \
